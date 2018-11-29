77bee886cedd2279d30b2872a39b7eb7d5d49c48
Skyla Qian <10014679@sbstudents.org>
2018-11-29 12:25:43 -0500

    Method 1
    Making convertToArrayList which changes an int to and ArrayList

--- Day2.java
+++ Day2.java
@@ -1,33 +1,29 @@
+import java.util.ArrayList;
 public class Day2{
  	public static void main(String[]args){
-
+		int num = 8950;
+		System.out.println(convertToArrayList(num));
  	}
+
  	public static ArrayList<Integer> convertToArrayList(int x){
 		System.out.println("Method 1");
 		ArrayList<Integer> list = new ArrayList<>();
 		if(x<10000 && x>=1000){
 			list.add(x/1000);
 			x = x%1000;
-			list.add(x/100);
-			x = x%100;
-			list.add(x/10);
-			x = x%10;
-			list.add(x);
 		}
 		if(x<1000 && x>=100){
 			list.add(x/100);
 			x = x%100;
-			list.add(x/10);
-			x = x%10;
-			list.add(x);
 		}
 		if(x<100 && x>=10){
 			list.add(x/10);
 			x = x%10;
-			list.add(x);
 		}
 		if(x<10 && x>= 0)
 			list.add(x);
+
 		return list;
 	}
+
  }
