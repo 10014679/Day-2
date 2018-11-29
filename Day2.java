94ee97c74a7bc8c9f87b6e64fbdc0f7550ed0f73
SRajan13 <45398968+SRajan13@users.noreply.github.com>
2018-11-29 12:03:26 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,11 +1,5 @@
 public class Day2{
  	public static void main(String[]args){
- 		System.out.print("Skyla and ");
- 		System.out.println("Surya");
-
- 		System.out.println("Red is cool");
-
- 		System.out.println("Swagmoney Skyla");
 
  	}
  }
